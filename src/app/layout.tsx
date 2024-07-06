b01204514ee1a6088a4e9e5e49794387dd3312a1
Sanele Hlongwane <sanelehlongwane61@gmail.com>
2024-07-06 13:54:18 +0200

    fixed Navbar spelling error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
-import Header from '../components/NavBar';
+import Header from '../components/Navbar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from '../components/Footer';
