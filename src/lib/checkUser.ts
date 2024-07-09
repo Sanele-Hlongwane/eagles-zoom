7ac8625e86d9244b08f6192083e78ede392152ad
Masumpa Hlongwane <135650019+ThriveMentor@users.noreply.github.com>
2024-07-09 15:14:54 +0200

    Update checkUser.ts

--- src/lib/checkUser.ts
+++ src/lib/checkUser.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export const checkUser = async (selectedRole: string | null) => {
+export const checkUser = async (selectedRole: string | null = null) => {
   const user = await currentUser();
 
   if (!user) {
@@ -15,7 +15,7 @@ export const checkUser = async (selectedRole: string | null) => {
       clerkId: user.id,
     },
     include: {
-      role: true, // Include the role in the query
+      role: true,
     },
   });
 
@@ -28,7 +28,7 @@ export const checkUser = async (selectedRole: string | null) => {
       email: user.emailAddresses[0].emailAddress,
     },
     include: {
-      role: true, // Include the role in the query
+      role: true,
     },
   });
 
@@ -42,25 +42,23 @@ export const checkUser = async (selectedRole: string | null) => {
         name: `${user.firstName} ${user.lastName}`,
         imageUrl: user.imageUrl,
         role: {
-          connect: { id: selectedRole || 'defaultRoleId' }, // Connect to selected role or default role ID
+          connect: { id: selectedRole || 'defaultRoleId' },
         },
       },
       include: {
-        role: true, // Include the role in the query
+        role: true,
       },
     });
 
     return loggedInUser;
   }
 
-  // Attempt to find the default role
   let defaultRole = await prisma.role.findUnique({
     where: {
       name: 'default',
     },
   });
 
-  // If default role doesn't exist, create it
   if (!defaultRole) {
     defaultRole = await prisma.role.create({
       data: {
@@ -76,11 +74,11 @@ export const checkUser = async (selectedRole: string | null) => {
       imageUrl: user.imageUrl,
       email: user.emailAddresses[0].emailAddress,
       role: {
-        connect: { id: selectedRole || defaultRole.id }, // Connect to selected role or default role ID
+        connect: { id: selectedRole || defaultRole.id },
       },
     },
     include: {
-      role: true, // Include the role in the query
+      role: true,
     },
   });
 
