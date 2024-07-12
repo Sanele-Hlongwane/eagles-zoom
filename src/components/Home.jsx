025941d4c0c1de0c6c4491b3a803d078c85b546f
Masumpa Hlongwane <135650019+ThriveMentor@users.noreply.github.com>
2024-07-13 00:43:45 +0200

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { useUser } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import { quickButtons } from "@/constants/constants";
@@ -15,11 +15,12 @@ import useGetCallsByUser from "@/hooks/useGetCallsByUser";
 
 export const UpComingMeetingPreview = ({ title, time }) => {
   return (
-    <div className="border-2 rounded-md border-gray-300/30 p-2 w-full mx-3 mt-2">
-      <p id="title" className="text-xl text-white mb-1">
+    <div className="border-2 rounded-md border-[#fafafa66
+    ] p-2 w-full mx-3 mt-2">
+      <p id="title" className="text-xl mb-1">
         {title}
       </p>
-      <p id="time" className="text-sm text-gray-400">
+      <p id="time" className="text-sm">
         {time}
       </p>
     </div>
@@ -50,9 +51,10 @@ export default function Home() {
 
   const [dialogData, setDialogData] = useState({});
   const [meetingType, setMeetingType] = useState(undefined);
+  /// meeting type should be either scheduleMeeting | joiningMeeting | instantMeeting | undefined (Default)
 
   return (
-    <div className="h-full flex flex-col justify-start items-center px-8 py-4 gap-y-10 bg-gray-900 text-white">
+    <div className="h-full flex flex-col justify-start items-center px-8 py-4 gap-y-10">
       <Modal
         isOpen={
           (meetingType == "scheduleMeeting") |
@@ -64,19 +66,25 @@ export default function Home() {
         type={dialogData.type}
         onClose={() => setMeetingType(undefined)}
       />
-      <div className="relative overflow-hidden w-full">
+      <div className="relative overflow-hidden w-full ">
         <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-green-400 opacity-20 rounded-lg"></div>
-        <div className="relative bg-gray-800 bg-opacity-30 backdrop-blur-md p-6 rounded-lg">
-          <h1 className="text-3xl font-bold">
+        <div className="relative bg-white bg-opacity-20 backdrop-blur-md p-6 rounded-lg">
+          <h1 className="text-3xl font-bold text-white">
             Hello, {user?.firstName} {user?.lastName}
           </h1>
-          <p className="text-gray-400">
-            {/* Placeholder text */}
+          <p className="text-gray-200">
+            {/* This is a glass effect created with Tailwind CSS. */}
           </p>
         </div>
       </div>
-      <div className="w-full max-w-[1200px] h-3/5 max-h-[65%] flex items-center justify-center">
-        <div className="grid w-1/2 place-items-center items-center max-w-[300px] gap-5 mr-10 grid-cols-2 grid-rows-2">
+      <div
+        id="secRow"
+        className="w-full max-w-[1200px] h-3/5 max-h-[65%] flex items-center justify-center"
+      >
+        <div
+          id="quickButton"
+          className="grid w-1/2 place-items-center items-center max-w-[300px] gap-5 mr-10 grid-cols-2 grid-rows-2 "
+        >
           {quickButtons.map(({ label, icon, handler, buttonLabel, type }) => {
             return (
               <QuickButton
@@ -84,7 +92,7 @@ export default function Home() {
                 icon={icon}
                 label={label}
                 handler={() => {
-                  setMeetingType(undefined);
+                  setMeetingType(undefined); // Make sure previous meeting type was wiped out
                   if (type == "record") {
                     router.push("/recordings");
                   }
@@ -94,30 +102,36 @@ export default function Home() {
                     type,
                     buttonLabel,
                   });
+                  console.log(type);
                 }}
               />
             );
           })}
         </div>
-        <div className="relative w-1/2 max-w-[800px] h-full">
+        <div
+          id="contentContainer"
+          className="relative w-1/2 max-w-[800px] h-full"
+        >
           <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-green-400 opacity-20 rounded-lg"></div>
-          <div className="relative bg-gray-800 bg-opacity-30 backdrop-blur-md p-6 rounded-t-lg">
-            <div className="h-32 text-3xl font-bold">
-              <div className="z-40 text-xl md:text-2xl lg:text-3xl absolute bottom-2 left-2 text-gray-100 backdrop-blur-sm backdrop-brightness-90 rounded-md p-2">
+          <div className="relative bg-white bg-opacity-20 backdrop-blur-md p-6 rounded-t-lg">
+            <div className="h-32 text-3xl font-bold text-white">
+              <div className="z-40 text-xl md:text-2xl lg:text-3xl  absolute bottom-2 left-2 text-gray-100 backdrop-blur-sm backdrop-brightness-90 rounded-md p-2">
                 <h1>{formattedTime}</h1>
                 <h1>{formattedDate}</h1>
               </div>
               <Image
+                fill
                 src="/preview.jpg"
                 alt="preview"
-                className="rounded-t-lg w-full h-32 object-cover"
-                layout="fill"
-                objectFit="cover"
+                className="rounded-t-lg w-full h-32 object-cover "
               />
             </div>
           </div>
-          <div className="w-full h-3/5 flex flex-col px-2 items-center justify-center overflow-scroll overflow-y-scroll">
-            {scheduled && <p className="text-gray-400">Upcoming meetings</p>}
+          <div
+            id="content"
+            className="w-full h-3/5 flex flex-col px-2 items-center justify-center overflow-scroll overflow-y-scroll"
+          >
+            {scheduled && <p className="text-gray-200">Upcoming meetings</p>}
             {scheduled ? (
               scheduled
                 .sort(
@@ -139,7 +153,7 @@ export default function Home() {
                   );
                 })
             ) : !isLoading ? (
-              <p className="text-gray-400">No upcoming meeting.</p>
+              <p className="text-gray-200">No upcoming meeting.</p>
             ) : (
               ""
             )}
